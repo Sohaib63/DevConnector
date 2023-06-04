c7f4b4411512b957b816e42fbd5d90dd5996567d
Sohaib63 <sohaibhafiz63@gmail.com>
2023-06-05 01:37:44 +0500

    Added model and check post req for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,14 @@ const router = express.Router();
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("User route"));
+// router.get("/", (req, res) => res.send("User route"));
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("User route");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ connectDB(); //Connect Database
 
 app.get("/", (req, res) => res.send("API Running"));
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
